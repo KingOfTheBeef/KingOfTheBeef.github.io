0455300ca657f32acaf85a3c08648128b97ccd4f
KingOfTheBeef <jonathan.longman@live.com.au>
2017-12-07 10:36:55 +0000

    Update navMenu.js

--- js/navMenu.js
+++ js/navMenu.js
@@ -5,4 +5,5 @@ document.getElementById("navMenu").innerHTML = "<ul>" +
 												"<li><a href='./events.html'>Events</a></li>" +
 												"<li><a href='./battleroyale.html'>Battle Royale Blog</a></li>" +
 												"<li><a href='./prom.html'>Prom Showcase!</a></li>" +
+												"<li><a href='./coursework.html'>A-Level Coursework</a></li>"
 												"</ul>";
